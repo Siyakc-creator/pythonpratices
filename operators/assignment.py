3c1346c502743ddf31c62d9c9c0cda040302ef15
Shreya karki <siyakc47@gmail.com>
2026-05-29 20:21:57 +0545

    Add operators folder

---
+++ operators/assignment.py
@@ -0,0 +1,40 @@
+# Imagine a variable named score starts with a value of 10.
+# If you apply the operation score += 5, what is the new value inside score?
+
+score = 10 
+score+=5
+print(score)
+
+
+# A variable named energy starts at 6.
+# If you apply the operation energy *= 3, what value does energy hold now?
+
+energy = 6
+energy*=3
+print(energy)
+
+
+# A variable named y starts with a value of 10.
+# If you see the expression y *= 2 + 3, remember that the computer evaluates the entire right side before applying the assignment shortcut.
+# Do you multiply by 2 first, or do you add $2 + 3$ first? What is the final value of y?
+# this gives error
+
+# y = 10 
+# y*2= 6
+# print(y)
+
+
+# A variable named items starts at 14.
+# If you apply the operation items //= 4, what whole number is left inside items?
+
+starts = 14
+starts//=4
+print(starts)
+
+
+# A variable named clock starts at 22.
+# If you apply the operation clock %= 5, what is the final leftover integer stored in clock?
+
+clock = 22
+clock%= 5
+print(clock)
